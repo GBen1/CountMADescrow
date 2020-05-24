21accd3f611c40245d9b1e946f6d4284c9a7f5c5
GBen1 <31903813+GBen1@users.noreply.github.com>
2020-05-24 04:25:20 +0200

    Create displayblockinfo.sh

---
+++ displayblockinfo.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+yel='\e[1;33m'
+neutre='\e[0;m'
+gr='\e[1;32m'
+red='\e[1;31m'
+bl='\e[1;36m'
+flred='\e[1;41m'
+
+$blockdisplay=0
+while ((blockdisplay < 1))
+do
+clear
+echo -e "${yel}Enter the number of the block you want to display:${neutre}" && read blockdisplay
+blockdisplay=$(echo $blockdisplay | cut -d "." -f 1 | cut -d "," -f 1 | tr -d [a-zA-Z]| sed -n '/^[[:digit:]]*$/p' )
+done
+
+cd
+cd particlcore
+
+
+
+
+blockhash=$(./particl-cli getblockstats $blockdisplay | grep blockhash | sed 's/.* //' | sed 's/"//' | sed 's/"//' | sed 's/,//')
+
+
+
+txcount=$(./particl-cli getblock $blockhash | cut -c5- | grep "^\"" | sed 's/"//' | sed 's/"//' | sed 's/,//' | wc -l)
+currenttx=1
+txcount=$(($txcount + 1))
+
+# for each tx in each block do:
+while [ "$txcount" -gt "$currenttx" ]
+do
+txid=$(./particl-cli getblock $blockhash | cut -c5- | grep "^\"" | sed 's/"//' | sed 's/"//' | sed 's/,//' | sed -n "$currenttx p")
+
+
+
+
+
+rawtx=$(./particl-cli getrawtransaction $txid) && decoderaw=$(./particl-cli decoderawtransaction $rawtx | cut -c12- | grep -E ^R | sed 's/"//') 
+#Display tx info
+echo $decoderaw
+
+
+
+
+currenttx=$(($currenttx + 1))
+done
+
+
