d9b701676f94314cb2e9e0f736ab625266cd1ed4
GBen1 <31903813+GBen1@users.noreply.github.com>
2020-05-24 06:22:22 +0200

    Update MAD2.sh

--- MAD2.sh
+++ MAD2.sh
@@ -8,6 +8,12 @@ red='\e[1;31m'
 bl='\e[1;36m'
 flred='\e[1;41m'
 
+
+cd
+cd particlcore
+
+rm ../CountMADescrow/lastblocksearch.txt
+
 currentblock=698350
 latestblock=$(./particl-cli getblockcount) 
 #for each block do...
@@ -15,23 +21,10 @@ while [ "$latestblock" -gt "$currentblock" ]
 do 
 
 
-blockdisplay=0
-while ((blockdisplay < 1))
-do
-clear
-echo -e "${yel}Enter the number of the block you want to display:${neutre}" && read blockdisplay
-blockdisplay=$(echo $blockdisplay | cut -d "." -f 1 | cut -d "," -f 1 | tr -d [a-zA-Z]| sed -n '/^[[:digit:]]*$/p' )
-done
-
-cd
-cd particlcore
-
-rm ../CountMADescrow/lastblocksearch.txt
+------
 
 
-blockhash=$(./particl-cli getblockstats $blockdisplay | grep blockhash | sed 's/.* //' | sed 's/"//' | sed 's/"//' | sed 's/,//')
-
-
+blockhash=$(./particl-cli getblockstats $currentblock | grep blockhash | sed 's/.* //' | sed 's/"//' | sed 's/"//' | sed 's/,//')
 
 txcount=$(./particl-cli getblock $blockhash | cut -c5- | grep "^\"" | sed 's/"//' | sed 's/"//' | sed 's/,//' | wc -l)
 currenttx=1
@@ -40,27 +33,28 @@ txcount=$(($txcount + 1))
 # for each tx in this block do:
 while [ "$txcount" -gt "$currenttx" ]
 do
+
+---
+
 txid=$(./particl-cli getblock $blockhash | cut -c5- | grep "^\"" | sed 's/"//' | sed 's/"//' | sed 's/,//' | sed -n "$currenttx p")
-
-
-
-
 
 rawtx=$(./particl-cli getrawtransaction $txid)
 
 ./particl-cli decoderawtransaction $rawtx >> ../CountMADescrow/lastblocksearch.txt
-./particl-cli decoderawtransaction $rawtx
+
+numad=$(cat ../CountMADescrow/lastblocksearch.txt | grep -A 10 blind | cut -c12- | grep -E ^R | sed -n '1~2p' | sed 's/"//' | wc -l)
+madtot=
 
 
 
 currenttx=$(($currenttx + 1))
+
+---
 done
-clear
- numad=$(cat ../CountMADescrow/lastblocksearch.txt | grep -A 10 blind | cut -c12- | grep -E ^R | sed -n '1~2p' | sed 's/"//' | wc -l)
- madlist=$(cat ../CountMADescrow/lastblocksearch.txt | grep -A 10 blind | cut -c12- | grep -E ^R | sed -n '1~2p' | sed 's/"//')
- 
-echo -e "${yel}$numad MADESCROWS CREATED IN THE BLOCK $blockdisplay: ${neutre}"
-echo ""
-echo -e "${gr}$madlist${neutre}"
-echo ""
+
+
 rm ../CountMADescrow/lastblocksearch.txt
+-----
+
+currentblock=$(($currentblock + 1)) 
+done
