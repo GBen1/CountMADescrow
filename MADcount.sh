e4ae9b0db456e788ab9f5972ae7e9265c7985bb0
GBen1 <31903813+GBen1@users.noreply.github.com>
2020-05-24 07:18:13 +0200

    Update MADcount.sh

--- MADcount.sh
+++ MADcount.sh
@@ -20,8 +20,7 @@ currentblock=0
 while ((currentblock < 1))
 do
 clear
-echo ""
-echo -e "${bl}The first MADescrow has been created during the block 444735 and we are at the block $latestblock ${neutre}"
+echo -e "${red}The first MADescrow has been created during the block 444735 and we are at the block $latestblock ${neutre}"
 echo -e "${yel}From which block do you want to count the MADescrow creations ?${neutre}" && read blockdisplay
 blockdisplay=$(echo $blockdisplay | cut -d "." -f 1 | cut -d "," -f 1 | tr -d [a-zA-Z]| sed -n '/^[[:digit:]]*$/p' )
 done
