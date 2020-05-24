c3241760b7c6bb6e5ecaaff2d40d91575cd70f38
GBen1 <31903813+GBen1@users.noreply.github.com>
2020-05-24 04:13:25 +0200

    Update MAD.sh

--- MAD.sh
+++ MAD.sh
@@ -1,16 +1,41 @@
-block=518023
+#!/bin/bash
+
+#block=518023
 #we should start at 0 but madescrow in this block (518023)
 
 cd
 cd particlcore
 
-#need a big loop block++ until we reach the last one
+
+currentblock=698350
+latestblock=$(./particl-cli getblockcount) 
+#for each block do...
+while [ "$latestblock" -gt "$currentblock" ]
+do 
+
+#####
+
+
+
+
+
 blockhash=$(./particl-cli getblockstats $block | grep blockhash | sed 's/.* //' | sed 's/"//' | sed 's/"//' | sed 's/,//')
 
-# need small loop: sed 1p, sed 2p, sed 3p... for each tx in the block
-txid=$( ./particl-cli getblock $blockhash | cut -c5- | grep "^\"" | sed 's/"//' | sed 's/"//' | sed 's/,//' | sed -n "1p")
+# for each tx in each block do:
 
-rawtx=$(./particl-cli getrawtransaction $txid) && decoderaw=$(./particl-cli decoderawtransaction $rawtx | cut -c12- | grep -E ^P | sed 's/"//') 
+txcount=$(./particl-cli getblock $blockhash | cut -c5- | grep "^\"" | sed 's/"//' | sed 's/"//' | sed 's/,//' | wc -l)
+currenttx=1
+txcount=$(($txcount + 1))
+
+while [ "$txcount" -gt "$currenttx" ]
+do
+txid=$(./particl-cli getblock $blockhash | cut -c5- | grep "^\"" | sed 's/"//' | sed 's/"//' | sed 's/,//' | sed -n "$currenttx p")
+
+
+
+
+
+rawtx=$(./particl-cli getrawtransaction $txid) && decoderaw=$(./particl-cli decoderawtransaction $rawtx | cut -c12- | grep -E ^R | sed 's/"//') 
 #Display tx info
 echo $decoderaw
 
@@ -18,3 +43,15 @@ echo $decoderaw
 nbmadtxinthistx=$(echo "rawtx" | wc -l) 
 
 nbmadtot=$(nbmadtxinthistx + nbmadtot)
+
+
+
+
+#####
+
+currenttx=$(($currenttx + 1))
+done
+
+#####
+currentblock=$(($currentblock + 1)) 
+done
