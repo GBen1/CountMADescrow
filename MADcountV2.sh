f0d34ba87b544200ca28be6601c41180e96c0791
GBen1 <31903813+GBen1@users.noreply.github.com>
2020-05-26 22:05:00 +0200

    Update MADcountV2.sh

--- MADcountV2.sh
+++ MADcountV2.sh
@@ -196,7 +196,7 @@ multisigcount=$(($multisigcount + 1))
 done
 
 
-#increase the madblock counter if there are madescrows in this block
+#Increase madblock if there was a Madescrow in the last txid scanned
 madblock=$(printf '%.3f\n' "$(echo "$madtxid" "+" "$madblock" | bc -l )")
 madblock=$(echo "$madblock" | cut -d "." -f 1 | cut -d "," -f 1)
 
