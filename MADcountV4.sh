d6a37d3560bb3e4e8dcc3932dd57263dfc1abab0
GBen1 <31903813+GBen1@users.noreply.github.com>
2020-06-05 09:18:19 +0200

    Update MADcountV4.sh

--- MADcountV4.sh
+++ MADcountV4.sh
@@ -349,11 +349,11 @@ else
   if [[ "$madlist" -eq 0 ]] ; then
   mkdir ../CountMADescrow/MYGRAPHS 2>/dev/null
   mkdir ../CountMADescrow/MYGRAPHS/$date 2>/dev/null
-  echo -e "${gr}BLOCK ${neutre}${yel}$currentblock ${neutre}${gr}: $multisig1${neutre} ${bl}(RELEASED : $getblockmadtx1)${neutre} ${red}(POTENTIAL FAKE : LIFETIME = $difftx) ${neutre}" >> ../CountMADescrow/MYGRAPHS/$date/madlist.txt
+  echo -e "${gr}BLOCK ${neutre}${yel}$currentblock ${neutre}${gr}: $multisig1${neutre} ${bl}(RELEASED : $getblockmadtx1)${neutre} ${red}(POTENTIAL FAKE : LIFETIME = $difftx)${neutre}" >> ../CountMADescrow/MYGRAPHS/$date/madlist.txt
   isfake=$(($isfake + 1))
   madlist=$(($madlist + 1)) 
   else
-  echo -e "${gr}BLOCK ${neutre}${yel}$currentblock ${neutre}${gr}: $multisig1${neutre} ${bl}(RELEASED : $getblockmadtx1)${neutre} ${red}(POTENTIAL FAKE : LIFETIME = $difftx) ${neutre}" >> ../CountMADescrow/MYGRAPHS/$date/madlist.txt
+  echo -e "${gr}BLOCK ${neutre}${yel}$currentblock ${neutre}${gr}: $multisig1${neutre} ${bl}(RELEASED : $getblockmadtx1)${neutre} ${red}(POTENTIAL FAKE : LIFETIME = $difftx)${neutre}" >> ../CountMADescrow/MYGRAPHS/$date/madlist.txt
   isfake=$(($isfake + 1))
   fi
 
@@ -361,10 +361,10 @@ else
   if [[ "$madlist" -eq 0 ]] ; then
   mkdir ../CountMADescrow/MYGRAPHS 2>/dev/null
   mkdir ../CountMADescrow/MYGRAPHS/$date 2>/dev/null
-  echo -e "${gr}BLOCK ${neutre}${yel}$currentblock ${neutre}${gr}: $multisig1${neutre}${bl} (RELEASED : $getblockmadtx1)${neutre}" >> ../CountMADescrow/MYGRAPHS/$date/madlist.txt
+  echo -e "${gr}BLOCK ${neutre}${yel}$currentblock ${neutre}${gr}: $multisig1${neutre}${bl} (RELEASED : $getblockmadtx1)${neutre} ${gr}(LIFETIME = $difftx)${neutre}" >> ../CountMADescrow/MYGRAPHS/$date/madlist.txt
   madlist=$(($madlist + 1)) 
   else
-  echo -e "${gr}BLOCK ${neutre}${yel}$currentblock ${neutre}${gr}: $multisig1${neutre}${bl} (RELEASED : $getblockmadtx1)${neutre}" >> ../CountMADescrow/MYGRAPHS/$date/madlist.txt
+  echo -e "${gr}BLOCK ${neutre}${yel}$currentblock ${neutre}${gr}: $multisig1${neutre}${bl} (RELEASED : $getblockmadtx1)${neutre} ${gr}(LIFETIME = $difftx)${neutre}" >> ../CountMADescrow/MYGRAPHS/$date/madlist.txt
   fi
  fi
 fi
